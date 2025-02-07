9b78df6ea674230a122a1f4c19d23276ee5422ef
adhikareeprayush <adhikareeprayush@gmail.com>
2025-02-07 13:55:00 +0545

    chhap01

---
+++ Week1/Day1/Day2/Chap01/basic.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // include directives
+
+// main function
+int main() {
+    std::cout << "Hello World!\n"; // statement
+    return 0;
+}
---
+++ Week1/Day1/Day2/Chap01/basic2.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // include directives 
+
+// main function
+int main() {
+    const char* hello {"Hello World!\n"}; // variable
+    std::cout << hello; // statement
+    return 0;
+}
